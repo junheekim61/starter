0d2b34f57210cd657c00a2127acf088a5ce36273
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-01-09 01:25:04 +0900

    starter 셋팅(5)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Reset } from 'styled-reset';
+import * as React from 'react';
+import Header from './components/Header';
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div className="root-wrap">
+    <>
+      <Reset />
       <Router>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </Router>
-    </div>
+    </>
   );
 }
